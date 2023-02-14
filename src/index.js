c21aa997f6a58698311f7e53708ca11bf6e047eb
Vitaly <nelyub86@ukr.net>
2023-02-14 13:54:18 +0200

    prgress

--- src/index.js
+++ src/index.js
@@ -6,17 +6,22 @@ const formInputValue = document.querySelector('.search-form input');
 const cardList = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
 
-// console.log(formInputValue.value);
+let page = 1;
+let inputCurrentValue = '';
+
 form.addEventListener('submit', getValue);
-// loadMore.addEventListener('click', loadMoreCards)
+loadMoreBtn.addEventListener('click', loadMoreCards);
 
 function getValue(e) {
   e.preventDefault();
-  const URL = `https://pixabay.com/api/?key=33583955-ce9811140fd4e045deb42856a&q=${formInputValue.value}&image_type=photo&orientation=horizontal&safesearch=true&per_page=4&page=1`;
+  let URL = `https://pixabay.com/api/?key=33583955-ce9811140fd4e045deb42856a&q=${formInputValue.value}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`;
 
   axios.get(URL).then(response => {
     response.data.hits;
     console.log(response.data.hits);
+    inputCurrentValue = formInputValue.value;
+    page += 1;
+    console.log(page);
     if (response.data.hits.length === 0 || formInputValue.value === '') {
       Notiflix.Notify.failure(
         'Sorry, there are no images matching your search query. Please try again.'
@@ -47,5 +52,21 @@ function renderGallery(hits) {
     </p>
   </div>
 </div>`;
+  });
+}
+
+function loadMoreCards() {
+  formInputValue.value = inputCurrentValue;
+  let URL = `https://pixabay.com/api/?key=33583955-ce9811140fd4e045deb42856a&q=${formInputValue.value}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`;
+  axios.get(URL).then(response => {
+    response.data.hits;
+    console.log(response.data.hits);
+    page += 1;
+    console.log(page);
+    renderGallery(response.data.hits);
+    formInputValue.value = '';
+    if (response.data.hits.length === 0) {
+      Notiflix.Notify.warning('This is all photo');
+    }
   });
 }
